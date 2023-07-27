7ec9ef444c9f1dbba5019dc4c84f3b4635bc2357
Ifedayo Awoniyi <awoniyiifedayopeter@gmail.com>
2023-07-27 17:35:54 +0100

    feat: custom chatbot application

---
+++ src/test/java/com/app/CustomChatbot/CustomChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.CustomChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
